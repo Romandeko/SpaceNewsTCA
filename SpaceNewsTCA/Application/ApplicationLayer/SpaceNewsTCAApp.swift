28d83bb3fa783429f2e1e502e2fdbdc7e72e9c36
romandenisenko <sir.roma-denisenko@yandex.ru>
2023-11-05 21:36:49 +0300

    Initial commit

---
+++ SpaceNewsTCA/Application/ApplicationLayer/SpaceNewsTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsTCAApp.swift
+//  SpaceNewsTCA
+//
+//  Created by Роман Денисенко on 5.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
